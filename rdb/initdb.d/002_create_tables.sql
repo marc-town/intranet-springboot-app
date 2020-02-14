38fcdc364f76447a194123af2d4e4b3fb17b67d4
marc-town <asamin.itconsultant@gmail.com>
2020-02-14 23:55:17 +0900

    勤怠管理用テーブル作成

--- rdb/initdb.d/002_create_tables.sql
+++ rdb/initdb.d/002_create_tables.sql
@@ -8,6 +8,7 @@ DROP TABLE IF EXISTS `m_department`;
 DROP TABLE IF EXISTS `m_position`;
 DROP TABLE IF EXISTS `m_grade`;
 DROP TABLE IF EXISTS `t_attendance`;
+DROP TABLE IF EXISTS `m_absence_type`;
 DROP TABLE IF EXISTS `m_attendance_setting`;
 DROP TABLE IF EXISTS `m_staff`;
 
@@ -26,7 +27,7 @@ CREATE TABLE `m_staff`
 CREATE TABLE `m_department`
 (
     `department_id` int not null auto_increment comment '社員ID',
-    `department` varchar(50) comment '部署名',
+    `department_name` varchar(50) comment '部署名',
     `description` varchar(255) comment '説明文',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
@@ -36,7 +37,7 @@ CREATE TABLE `m_department`
 CREATE TABLE `m_grade`
 (
     `grade_id` int not null auto_increment comment 'グレードID',
-    `grade` varchar(50) comment 'グレード名',
+    `grade_name` varchar(50) comment 'グレード名',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
     PRIMARY KEY(`grade_id`)
@@ -45,15 +46,25 @@ CREATE TABLE `m_grade`
 CREATE TABLE `m_position`
 (
     `position_id` int not null auto_increment comment '役職ID',
-    `position` varchar(50) comment '役職名',
+    `position_name` varchar(50) comment '役職名',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
     PRIMARY KEY(`position_id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
+CREATE TABLE `m_absence_type`
+(
+    `absence_type_id` int not null auto_increment comment 'ID',
+    `absence_name` varchar(255) comment '欠勤種別名',
+    `create_at` timestamp not null default current_timestamp comment '登録日',
+    `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
+    PRIMARY KEY(`absence_type_id`),
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 CREATE TABLE `m_staff_basic_info`
 (
-    `id` int not null auto_increment comment 'ID',
+    `m_staff_basic_info_id` int not null auto_increment comment 'ID',
     `staff_id` int not null comment '社員ID',
     `name` varchar(100) comment '社員名',
     `name_kana` varchar(100) comment '社員名（かな）',
@@ -66,7 +77,7 @@ CREATE TABLE `m_staff_basic_info`
     `grade_id` int default 0 comment '階級ID',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
-    PRIMARY KEY(`id`),
+    PRIMARY KEY(`m_staff_basic_info_id`),
     CONSTRAINT fk_basic_info_staff_id  -- 制約の名前
         FOREIGN KEY (staff_id)  -- 外部キーに設定するカラム名
         REFERENCES m_staff (staff_id)  -- 参照するテーブルとカラム
@@ -74,14 +85,14 @@ CREATE TABLE `m_staff_basic_info`
 
 CREATE TABLE `m_staff_detail_info`
 (
-    `id` int not null auto_increment comment 'ID',
+    `staff_detail_info_id` int not null auto_increment comment 'ID',
     `staff_id` int not null comment '社員ID',
     `hobby` varchar(255) comment '趣味',
     `interest` varchar(255) comment '興味のある仕事',
     `project_summary` text comment '案件概要',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
-    PRIMARY KEY(`id`),
+    PRIMARY KEY(`staff_detail_info_id`),
     CONSTRAINT fk_detail_info_staff_id  -- 制約の名前
         FOREIGN KEY (staff_id)  -- 外部キーに設定するカラム名
         REFERENCES m_staff (staff_id)  -- 参照するテーブルとカラム
@@ -90,18 +101,32 @@ CREATE TABLE `m_staff_detail_info`
 CREATE TABLE `t_attendance`
 (
     `attendance_id` int not null auto_increment comment '勤怠ID',
+    `year_month` char(6) not null default '190001' comment '年月',
+    `day` char(2) not null default '01' comment '日付',
     `staff_id` int not null comment '社員ID',
+    `start_time` char(5) default '09:00' comment '始業時間',
+    `end_time` char(5) default '18:00' comment '就業時間',
+    `absence_type` int comment '欠勤種別',
+    `absence_reason` varchar(255) comment '欠勤理由',
+    `working_time` decimal(4, 2) comment '労働時間',
+    `night_time` decimal(4, 2) comment '夜間時間',
+    `operating_expenses` int comment '営業費用',
+    `section` varchar(255) comment '営業区間',
+    `remarks` varchar(255) comment '備考',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
     PRIMARY KEY(`attendance_id`),
     CONSTRAINT fk_attendance_staff_id  -- 制約の名前
         FOREIGN KEY (staff_id)  -- 外部キーに設定するカラム名
         REFERENCES m_staff (staff_id)  -- 参照するテーブルとカラム
+    CONSTRAINT fk_attendance_absence_type_id  -- 制約の名前
+        FOREIGN KEY (absence_type_id)  -- 外部キーに設定するカラム名
+        REFERENCES m_absence_type (absence_type_id)  -- 参照するテーブルとカラム
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `m_attendance_setting`
 (
-    `id` int not null auto_increment comment 'ID',
+    `attendance_setting_id` int not null auto_increment comment 'ID',
     `staff_id` int not null comment '社員ID',
     `home_nearest` varchar(100) comment '自宅最寄り駅',
     `working_nearest` varchar(100) comment '現場最寄り駅',
@@ -111,7 +136,7 @@ CREATE TABLE `m_attendance_setting`
     `regular_end_time` varchar(5) default '17:30' comment '就業定時',
     `create_at` timestamp not null default current_timestamp comment '登録日',
     `update_at` timestamp not null default current_timestamp on update current_timestamp comment '更新日',
-    PRIMARY KEY(`id`),
+    PRIMARY KEY(`attendance_setting_id`),
     CONSTRAINT fk_attendance_setting_staff_id  -- 制約の名前
         FOREIGN KEY (staff_id)  -- 外部キーに設定するカラム名
         REFERENCES m_staff (staff_id)  -- 参照するテーブルとカラム
@@ -120,7 +145,7 @@ CREATE TABLE `m_attendance_setting`
 ---- insert ----
 
 -- m_department
-INSERT INTO m_department (department) VALUES
+INSERT INTO m_department (department_name) VALUES
 ("開発事業部"),
 ("インフラ事業部"),
 ("営業部"),
@@ -129,14 +154,14 @@ INSERT INTO m_department (department) VALUES
 ;
 
 -- m_position
-INSERT INTO m_position (position) VALUES
+INSERT INTO m_position (position_name) VALUES
 ("会長"),
 ("平社員"),
 ("雑用")
 ;
 
 -- m_grade
-INSERT INTO m_grade (grade) VALUES
+INSERT INTO m_grade (grade_name) VALUES
 ("Platinum"),
 ("Gold"),
 ("Silver"),
@@ -144,9 +169,18 @@ INSERT INTO m_grade (grade) VALUES
 ("Green")
 ;
 
+-- m_absence_type
+INSERT INTO m_absence_type (absence_type_name) VALUES
+("欠勤"),
+("代休"),
+("有給"),
+("忌引")
+;
+
 -- m_staff
 INSERT INTO m_staff (login_id, password) VALUES
-("admin", "admin")
+("admin", "admin"),
+("takenomikazuchi", "takenomikazuchi")
 ;
 
 -- m_staff_basic_info
