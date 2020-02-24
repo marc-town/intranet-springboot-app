ef33993af55d30d52e8731d5847568d0d35a1deb
marc-town <asamin.itconsultant@gmail.com>
2020-02-24 16:48:03 +0900

    多言語プラグイン追加

--- web-gui/intranet-front-web/src/main.js
+++ web-gui/intranet-front-web/src/main.js
@@ -9,6 +9,15 @@ import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+import VueI18n from 'vue-i18n'
+import messages from './i18n/messages.json'
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: navigator.language.split('-')[0],
+  fallbackLocale: 'ja',
+  messages
+});
+
 Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
@@ -29,5 +38,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
