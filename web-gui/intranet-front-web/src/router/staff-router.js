1e8c24e6726c90fb2a7e6ba3931617cbbe9d4bf1
marc-town <asamin.itconsultant@gmail.com>
2020-02-25 23:08:05 +0900

    sidebarリンク設定

--- web-gui/intranet-front-web/src/router/staff-router.js
+++ web-gui/intranet-front-web/src/router/staff-router.js
@@ -1,10 +1,11 @@
-import Staff from '@/components/pages/staffs/Staff'
+// import Staff from '@/components/pages/staffs/Staff'
+import Template from '@/components/parts/template/Template'
 
 import StaffList from '@/components/pages/staffs/staff_list/StaffList'
 
 export default {
   path: '/staffs',
-  component: Staff,
+  component: Template,
   children: [
     {
       path: '',
